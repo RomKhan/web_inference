145068437edaacf529376cd82f326ad58a0dfdd9
RomKhan <rokhan@edu.hse.ru>
2024-04-29 23:58:37 +0500

    bug fix

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -3,7 +3,6 @@ from PIL import Image
 from io import BytesIO
 import clip
 import base64
-import os
 import torch.nn.functional as F
 from models import ImageModel, CorrectionModel
 from sklearn.preprocessing import LabelEncoder
@@ -20,7 +19,6 @@ def init_encoder(key):
     encoder = LabelEncoder()
     path = f'{encoder_name}_{key}.npy'
     encoder.classes_ = np.load(path, allow_pickle=True)
-    # print(key, encoder.classes_, '\n')
     return encoder
 
 correction_model = torch.load(correct_model_path, map_location=device).to(device)
@@ -50,9 +48,15 @@ def prepare_data(json, images=None):
     X_num = torch.zeros((1, 13))
     for key in json:
         if key in cat_features:
-            X_cat[0][cat_features[key]] = cat_encode(key, json[key])
+            try:
+                X_cat[0][cat_features[key]] = cat_encode(key, json[key])
+            except:
+                X_cat[0][cat_features[key]] = 0
         elif key in num_features:
-            X_num[0][num_features[key]] = float(json[key])
+            try:
+                X_num[0][num_features[key]] = float(json[key])
+            except:
+                X_num[0][num_features[key]] = 0.0
     if 'address' in json and len(json['address']) > 0:
         result = dadata.suggest("address", json['address'])
         if len(result) > 0:
@@ -60,7 +64,9 @@ def prepare_data(json, images=None):
             X_num[0][num_features['latitude']] = float(lat)
             X_num[0][num_features['longitude']] = float(lon)
         else:
-            raise Exception('не получилось найти координаты, напишите аддресс в дргой форме')
+            X_num[0][num_features['latitude']] = 55.721240
+            X_num[0][num_features['longitude']] = 37.576825
+            # raise Exception('не получилось найти координаты, напишите аддресс в дргой форме')
 
     image_embedding = torch.zeros(image_embedding_size)
     if len(images) > 0:
